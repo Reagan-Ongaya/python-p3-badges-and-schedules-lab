0cec001ae8d61f35111d9b0be2d0dea0ed3f8215
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-03 16:23:11 +0300

    Assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return ["Hello, {}! You'll be assigned to room {}!".format(name, index + 1) for index, name in enumerate(names)]
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for room in rooms:
+        print(room)
+
+names_list = ["Arel", "Carol"]
+printer(names_list)
